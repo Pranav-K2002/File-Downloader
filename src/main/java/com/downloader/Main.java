7935e95e82a2cd8bbe7686f767efd1753b14ec3b
Pranav-K2002 <pranav.kamath2002@gmail.com>
2025-02-01 19:21:59 +0530

    initial commit

---
+++ src/main/java/com/downloader/Main.java
@@ -0,0 +1,11 @@
+package com.downloader;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws MalformedURLException {
+        URL urlToDownload = new URL("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Jf0NnGpH2AhNM3BtwZufwwHaJ4%26pid%3DApi&f=1&ipt=95dfd920958e38d678178d986fbc8f2584e1bb7af1268d541a73b86ecb0e8966&ipo=images");
+
+    }
+}
